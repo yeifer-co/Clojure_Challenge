d03a5e4ca4d965a43ccff0e95832eb7921ee14fd
yeifer-co <developer@yeifer.co>
2023-11-04 17:58:52 -0500

    🔧(📦): Base configuration of idea project

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clojure_lein "0.1.0-SNAPSHOT"
+  :description "Learning Clojure Challenge project"
+  :url "https://github.com/yeifer-co/clojure_challenge"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot clojure-lein.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
