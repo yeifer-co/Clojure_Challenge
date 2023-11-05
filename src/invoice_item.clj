f99e19f32688b21798ac52980cbfbe3c5f17aa39
yeifer-co <developer@yeifer.co>
2023-11-04 22:46:46 -0500

    📃(🧩): Add docstring to describe functions

--- src/invoice_item.clj
+++ src/invoice_item.clj
@@ -17,21 +17,29 @@
 (def invoice (edn/read-string (slurp "invoice.edn")))
 
 ; Condition 1: At least have one item that has :iva 19%
-(defn condition-1 [item]
+(defn condition-1
+  "At least have one item that has :iva 19%"
+  [item]
   (some #(= 19 (:tax/rate %)) (:taxable/taxes item)))
 
 ; Condition 2: At least one item has retention :ret_fuente 1%
-(defn condition-2 [item]
+(defn condition-2
+  "At least one item has retention :ret_fuente 1%"
+  [item]
   (some #(= 1 (:retention/rate %)) (:retentionable/retentions item)))
 
-(defn check-conditions [item]
+(defn check-conditions
+  "Check if an item satisfies the conditions"
+  [item]
   ; Every item must satisfy EXACTLY one of the above two conditions.
   (if (condition-1 item)
     (not (condition-2 item))
     (condition-2 item)))
 
 ; Filter invoice items by conditions
-(defn filter-by-conditions [invoice]
+(defn filter-by-conditions
+  "Filter invoice items by conditions"
+  [invoice]
   ; Use ->> to thread the invoice items through the filter
   (->> invoice ; thread operator allows to pass the result of the previous expression as the last argument of the next expression
        :invoice/items
