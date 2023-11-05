3119ada224d7a4753f265c5ad6c0f5a7faf90731
yeifer-co <developer@yeifer.co>
2023-11-04 23:00:42 -0500

    🔁(🧩): Split invoice play arround in a separated file

--- src/invoice_item.clj
+++ src/invoice_item.clj
@@ -1,5 +1,4 @@
 (ns invoice-item)
-(require '[clojure.edn :as edn])
 
 (defn- discount-factor [{:invoice-item/keys [discount-rate]
                          :or                {discount-rate 0}}]
@@ -10,42 +9,3 @@
     :as                item
     :or                {discount-rate 0}}]
   (* precise-price precise-quantity (discount-factor item)))
-
-;^; Problem 1 Solution
-
-; Load invoice to play around with it
-(def invoice (edn/read-string (slurp "invoice.edn")))
-
-; Condition 1: At least have one item that has :iva 19%
-(defn condition-1?
-  "At least have one item that has :iva 19%"
-  [item]
-  (some #(= 19 (:tax/rate %)) (:taxable/taxes item)))
-
-; Condition 2: At least one item has retention :ret_fuente 1%
-(defn condition-2?
-  "At least one item has retention :ret_fuente 1%"
-  [item]
-  (some #(= 1 (:retention/rate %)) (:retentionable/retentions item)))
-(def not-condition-2?
-  "No item has retention :ret_fuente 1%"
-  (complement condition-2?))
-
-(defn check-conditions
-  "Check if an item satisfies the conditions"
-  [item]
-  ; Every item must satisfy EXACTLY one of the above two conditions.
-  (if (condition-1? item)
-    (not-condition-2? item)
-    (condition-2? item)))
-
-; Filter invoice items by conditions
-(defn filter-by-conditions
-  "Filter invoice items by conditions"
-  [invoice]
-  ; Use ->> to thread the invoice items through the filter
-  (->> invoice ; thread operator allows to pass the result of the previous expression as the last argument of the next expression
-       :invoice/items
-       (filter check-conditions)))
-
-(def result (filter-by-conditions invoice))
---
+++ src/invoice_play_around.clj
@@ -0,0 +1,44 @@
+(ns invoice
+  (:require [clojure.edn :as edn]))
+
+;^; Problem 1 Solution
+
+; Load invoice to play around with it
+(def invoice (edn/read-string (slurp "invoice.edn")))
+
+; Condition 1: At least have one item that has :iva 19%
+(defn condition-1?
+  "At least have one item that has :iva 19%"
+  [item]
+  (some #(= 19 (:tax/rate %)) (:taxable/taxes item)))
+
+; Condition 2: At least one item has retention :ret_fuente 1%
+(defn condition-2?
+  "At least one item has retention :ret_fuente 1%"
+  [item]
+  (some #(= 1 (:retention/rate %)) (:retentionable/retentions item)))
+(def not-condition-2?
+  "No item has retention :ret_fuente 1%"
+  (complement condition-2?))
+
+(defn check-conditions
+  "Check if an item satisfies the conditions"
+  [item]
+  ; Every item must satisfy EXACTLY one of the above two conditions.
+  (if (condition-1? item)
+    (not-condition-2? item)
+    (condition-2? item)))
+
+; Filter invoice items by conditions
+(defn filter-by-conditions
+  "Filter invoice items by conditions"
+  [invoice]
+  ; Use ->> to thread the invoice items through the filter
+  (->> invoice ; thread operator allows to pass the result of the previous expression as the last argument of the next expression
+       :invoice/items
+       (filter check-conditions)))
+
+(def result (filter-by-conditions invoice))
+
+; Print the result
+(println result)
