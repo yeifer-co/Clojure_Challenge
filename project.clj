6ddf051e84ebfad17b88dae8c7e720259a4a7f3d
yeifer-co <developer@yeifer.co>
2023-11-04 20:39:55 -0500

    🔁(📦): Change project name to match repo name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure_lein "0.1.0-SNAPSHOT"
+(defproject clojure_challenge "0.1.0-SNAPSHOT"
   :description "Learning Clojure Challenge project"
   :url "https://github.com/yeifer-co/clojure_challenge"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
