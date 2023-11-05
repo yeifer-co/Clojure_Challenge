92ebce779f28cf009766f2978ad7e145a0f17a83
yeifer-co <developer@yeifer.co>
2023-11-05 08:45:10 -0500

    🐞(🧩): Fix key destructuring in invoice item

--- src/invoice_item.clj
+++ src/invoice_item.clj
@@ -1,11 +1,11 @@
 (ns invoice-item)
 
-(defn- discount-factor [{:invoice-item/keys [discount-rate]
+(defn- discount-factor [{:keys [discount-rate]
                          :or                {discount-rate 0}}]
   (- 1 (/ discount-rate 100.0)))
 
 (defn subtotal
-  [{:invoice-item/keys [precise-quantity precise-price discount-rate]
+  [{:keys [precise-quantity precise-price discount-rate]
     :as                item
     :or                {discount-rate 0}}]
   (* precise-price precise-quantity (discount-factor item)))
