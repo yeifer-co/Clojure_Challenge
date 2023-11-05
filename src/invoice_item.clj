28c9cdf930f007793bec0261d60070a3e9be1b11
yeifer-co <developer@yeifer.co>
2023-11-04 22:19:42 -0500

    🌟(🧩): Create function to filter invoice by conditions

--- src/invoice_item.clj
+++ src/invoice_item.clj
@@ -1,4 +1,5 @@
 (ns invoice-item)
+(require '[clojure.edn :as edn])
 
 (defn- discount-factor [{:invoice-item/keys [discount-rate]
                          :or                {discount-rate 0}}]
@@ -10,3 +11,13 @@
     :or                {discount-rate 0}}]
   (* precise-price precise-quantity (discount-factor item)))
 
+(def invoice (edn/read-string (slurp "invoice.edn")))
+
+(defn filter-by-conditions [invoice]
+  (->> invoice
+       :invoice/items
+       (filter (fn [item]
+                 (or (some #(= 19 (:tax/rate %)) (:taxable/taxes item))
+                     (some #(= 1 (:retention/rate %)) (:retentionable/retentions item)))))))
+
+(def result (filter-by-conditions invoice))
